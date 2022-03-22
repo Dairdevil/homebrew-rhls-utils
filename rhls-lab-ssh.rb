9ca2541a8423f9bacf957ce169bb06fe8559aefc
Alasdair Preston <alasdair.preston@gmail.com>
2022-03-22 15:30:49 +0000

    Initial rhls-lab-ssh formulae

---
+++ rhls-lab-ssh.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class RHLSLabSSH < Formula
+  desc "Util script for configuring and connecting to an RHLS lab via SSH"
+  homepage "https://github.com/Dairdevil/rhls-lab-ssh"
+  url "https://github.com/Dairdevil/rhls-lab-ssh/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "bd5d8b46ab5a6d2d27094d4661af882284bc621baf268aa8a3f258e5fdb3a002"
+  license ""
+
+  depends_on "bash" => :run
+
+  def install
+    bin.install "bin/rhls-lab"
+  end
+
+  test do
+  end
+
+end
