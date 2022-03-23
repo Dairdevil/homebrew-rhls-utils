903aa7126685e68a20f1c2c4a96bc9140718ce31
Alasdair Preston <alasdair.preston@gmail.com>
2022-03-23 17:13:28 +0000

    Upversion rhls-lab-ssh to v1.0.0

--- rhls-lab-ssh.rb
+++ rhls-lab-ssh.rb
@@ -4,8 +4,8 @@
 class RhlsLabSsh < Formula
   desc "Util script for configuring and connecting to an RHLS lab via SSH"
   homepage "https://github.com/Dairdevil/rhls-lab-ssh"
-  url "https://github.com/Dairdevil/rhls-lab-ssh/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "3d9f3bed42e0e959932a881cf0699304beb86a40be45c45aaa8914c49cfddd94"
+  url "https://github.com/Dairdevil/rhls-lab-ssh/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "563e0d9e5340516cde74b1b472f9dcb8e06ff5924e09e38b93fb29fd7575afb2"
   license ""
 
   depends_on "bash" => :run
