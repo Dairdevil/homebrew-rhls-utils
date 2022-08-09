353e3e0dd5ad56e124d7a1c576e387f0b7ba943b
Alasdair Preston <apreston@redhat.com>
2022-08-09 14:25:24 +0100

    Upversion rhls-lab-ssh to 1.0.2

--- rhls-lab-ssh.rb
+++ rhls-lab-ssh.rb
@@ -1,11 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class RhlsLabSsh < Formula
   desc "Util script for configuring and connecting to an RHLS lab via SSH"
   homepage "https://github.com/Dairdevil/rhls-lab-ssh"
-  url "https://github.com/Dairdevil/rhls-lab-ssh/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "336c201b6a1985712f62f7c7deb27f195d4499b22dea53cd6af205c4d331f72a"
+  url "https://github.com/Dairdevil/rhls-lab-ssh/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "562c429e7a34762bc6158f66ec63ae706ef6c455c314080bea28c0c07a031feb"
   license ""
 
   depends_on "bash" => :run
